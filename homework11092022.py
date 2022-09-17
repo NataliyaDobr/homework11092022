6446d8be5d231603604c2128a5addbf15cd623c6
NataliyaDobr <n.m.suvorova@mail.ru>
2022-09-17 20:48:18 +0300

    task 4

--- homework11092022.py
+++ homework11092022.py
@@ -7,16 +7,16 @@
 # print(round(num,dd))
 
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-num = int(input('Введите число '))
-print("делители: ", end=" ")
-for i in range(num-1,1,-1):
-    simple_num = 0
-    if (num%i == 0):
-        for j in range(i-1,1,-1):
-            if (i % j == 0):
-                simple_num = simple_num + 1
-        if (simple_num == 0):
-            print(i, end = " ")
+# num = int(input('Введите число '))
+# print("делители: ", end=" ")
+# for i in range(num-1,1,-1):
+#     simple_num = 0
+#     if (num%i == 0):
+#         for j in range(i-1,1,-1):
+#             if (i % j == 0):
+#                 simple_num = simple_num + 1
+#         if (simple_num == 0):
+#             print(i, end = " ")
 
 # 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 # list = [1,1,'abcd',2,3,3,4,4,5,5,'abcd',6,7,7,8,8,999]
@@ -24,3 +24,25 @@ for i in range(num-1,1,-1):
 # for elem in list:
 #     unic_list.add(elem)
 # print(unic_list)
+
+# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена 
+# и записать в файл многочлен степени k. Пример:- k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+from random import randint
+k = int(input('Введите натуральную степень k = '))
+koeff = []
+f = open('urav.txt','w')  # открытие в режиме записи
+for i in range(k):
+    koeff.append(randint(0,100))
+    f.write(str(koeff[i]))  # запись  файл
+    f.write(str('x^'))
+    f.write(str(k-i))
+    f.write(str('+'))
+last_index = randint(0,100)
+f.write(str(last_index))
+f.write(str('=0'))
+f.close()
+print(koeff)
+
+
+
+
